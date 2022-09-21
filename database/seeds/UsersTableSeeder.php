8633a189da76081497e5185b1f40c21c8051d1e0
markcaf <marcocafassog@gmail.com>
2022-09-22 00:16:17 +0200

    feat(seeds): add users table seeder with faker

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $myUser = new User();
+        $myUser->name = 'Admin';
+        $myUser->email = 'admin@laravel.it';
+        $myUser->password = Hash::make('12345678');
+        $myUser->save();
+
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+            $user->name = $faker->userName();
+            $user->email = $faker->unique()->email();
+            $user->password = Hash::make($faker->password());
+            $user->save();
+        }
+    }
+}
